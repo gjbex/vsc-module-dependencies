f7d5238acefc45f36f72ecb5237da0fa049d7783
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-03-10 19:37:24 +0100

    Added initialization of dependency table

--- lib/vsc/module/dependencies.py
+++ lib/vsc/module/dependencies.py
@@ -7,6 +7,7 @@ class ModuleDependencies(object):
            of modules as values'''
         self._conn = sqlite3.connect(':memory:')
         self._init_module_table(modules)
+        self._init_dependency_table(modules)
 
     def _init_module_table(self, modules):
         cursor = self._conn.cursor()
@@ -15,11 +16,12 @@ class ModuleDependencies(object):
         for module_list in modules.values():
             all_modules.update(module_list)
         cursor.execute(
-            '''CREATE TABLE modules
-                   (module_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    module_name TEXT NOT NULL,
-                    module_version TEXT NOT NULL,
-                    UNIQUE(module_name, module_version))''' 
+            '''CREATE TABLE modules (
+                   module_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   module_name TEXT NOT NULL,
+                   module_version TEXT NOT NULL,
+                   UNIQUE(module_name, module_version)
+               )''' 
         )
         insert_stmt = '''INSERT INTO modules
                              (module_name, module_version) VALUES (?, ?)'''
@@ -29,6 +31,46 @@ class ModuleDependencies(object):
         cursor.close()
         self._conn.commit()
 
+    def _init_dependency_table(self, modules):
+        cursor = self._conn.cursor()
+        cursor.execute(
+            '''CREATE TABLE dependencies (
+                   dependency_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   module_id INTEGER NOT NULL,
+                   depends_on_id INTEGER NOT NULL,
+                   FOREIGN KEY (module_id)
+                       REFERENCES modules (module_id),
+                   FOREIGN KEY (depends_on_id)
+                       REFERENCES modules (module_id),
+                   UNIQUE (module_id, depends_on_id)
+               )'''
+        )
+        for module, dependency_list in modules.iteritems():
+            module_id = self.get_module_id(module)
+            for depends_on in dependency_list:
+                depends_on_id = self.get_module_id(depends_on)
+                cursor.execute(
+                    '''INSERT INTO dependencies
+                           (module_id, depends_on_id) VALUES (?, ?)''',
+                    (module_id, depends_on_id)
+                )
+        cursor.close()
+        self._conn.commit()
+
+    def get_module_id(self, module):
+        module_name, module_version = module.split('/')
+        cursor = self._conn.cursor()
+        results = cursor.execute(
+            '''SELECT module_id FROM modules
+                   WHERE module_name = ? AND module_version = ?''',
+            (module_name, module_version)
+        )
+        module_id = results.fetchone()
+        if module_id:
+            return module_id[0]
+        else:
+            return None
+        
     def all_modules(self):
         modules = []
         cursor = self._conn.cursor()
